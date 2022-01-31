6bfa30bed55029914af23f7d398d5de2299ee157
Vladimir Kovalev <boban_envorter@mail.ru>
2022-01-31 20:40:37 +0300

    add helper

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,60 @@
+package page;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private final SelenideElement heading = $("[data-test-id=dashboard]");
+    private final SelenideElement firstCard = $$(".list__item").first();
+    private final SelenideElement secondCard = $$(".list__item").last();
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    private final SelenideElement firstButton = $$("[data-test-id='action-deposit']").first();
+    private final SelenideElement secondButton = $$("[data-test-id='action-deposit']").last();
+    private final SelenideElement reload = $("[data-test-id='action-reload']");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+    public TransferPage firstCardButton() {
+        firstButton.click();
+        return new TransferPage();
+    }
+
+    public TransferPage secondCardButton() {
+        secondButton.click();
+        return new TransferPage();
+    }
+
+    public int getFirstCardBalance() {
+        val text = firstCard.text();
+        return extractBalanceCard1(text);
+    }
+
+    private int extractBalanceCard1(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+    public int getSecondCardBalance() {
+        val text = secondCard.text();
+        return extractBalanceCard2(text);
+    }
+
+    private int extractBalanceCard2(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+
+}
+
---
+++ src/test/java/page/TransferPage.java
@@ -0,0 +1,25 @@
+package page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+
+    private final SelenideElement heading = $(withText("Пополнение карты"));
+    private final SelenideElement amount = $("[data-test-id='amount'] input");
+    private final SelenideElement from = $("[data-test-id='from'] input");
+    private final SelenideElement button = $("[data-test-id='action-transfer']");
+    private final SelenideElement cancelButton = $("[data-test-id='action-cancel']");
+    private final SelenideElement error = $("[data-test-id='error-notification']");
+
+    public TransferPage(){
+        heading.shouldBe(visible);
+    }
+
+    public DashboardPage transferForm(String sum, DataHelper.CardNumber cardNumber){
+
+    }
+}
